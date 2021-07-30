7b064109b3cbb0cf45d54691236c2704df41dd8e
Rogerio Pereira <hi@rogeriopereira.dev>
2021-07-30 17:35:01 -0400

    Aula: 02 - Criando Pacotes no Laravel

---
+++ packages/rogerio-pereira/especializati-microservices-common/src/Services/Traits/ConsumeExternalService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace RogerioPereira\EspecializatiMicroserviceCommon\Services\Traits;
+
+class ConsumeExternalService
+{
+    public function teste()
+    {
+        return 'teste package';
+    }
+}
